0bf54d752c286a74e79674ac697ab3b9130071b2
Asma Rafique <97357575+Asma-Rafique@users.noreply.github.com>
2024-07-13 00:52:11 +0500

    DB_url is change after after dockerizing files

--- DB/template.py
+++ DB/template.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-DATABASE_URL = 'postgresql://postgres:asmarafique@localhost:5432/template_database'
-
+DATABASE_URL = 'postgresql://postgres:asmarafique@db:5432/template_database'
+# DATABASE = os.getenv('DATABASE_URL')
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
